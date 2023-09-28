63bf489d46e058e2542f9d737f137ce7f0318272
Sun Jixiang <jixiang.sun@insa-lyon.fr>
2023-09-29 01:07:28 +0200

    用js插入jixiang

--- js/Namida.js
+++ js/Namida.js
@@ -1,8 +1,8 @@
-let n = document.createElement("p");
-n.innerText = "Jixiang";
-let edts = document.querySelectorAll("table.edt");
-let body = document.querySelector("body");
-console.log(edts);
-for (let edt of edts) {
-  body.insertBefore(n, edt);
+let h2s = document.querySelectorAll("h2");
+console.log(h2s);
+for (let h2 of h2s) {
+  h2.insertAdjacentHTML(
+    "afterend",
+    "<p>Cette interface a été embellie par Jixiang.</p>"
+  );
 }
