2b303b4dd8ba0570ba745a9caa86772afbcfaeae
Sun Jixiang <jixiang.sun@insa-lyon.fr>
2023-10-01 00:58:32 +0200

    课程表更新

--- js/Namida.js
+++ js/Namida.js
@@ -2,7 +2,7 @@ let h2s = document.querySelectorAll("h2");
 for (let h2 of h2s) {
   h2.insertAdjacentHTML(
     "afterend",
-    "<p>Cette interface a été embellie par Jixiang. L'EdT est mis à jour avec ADE à 1h14 le 29/09/2023.</p>"
+    "<p>Cette interface a été embellie par Jixiang. L'EdT est mis à jour avec ADE à 0h58 le 1/10/2023.</p>"
   );
 }
 
