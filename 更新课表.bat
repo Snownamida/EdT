159d3bc1bf5f7ceb87f09d352fe3e568338d5f7f
Sun Jixiang <jixiang.sun@insa-lyon.fr>
2023-10-01 15:12:29 +0200

    删除一堆没用的css

--- 更新课表.bat
+++ 更新课表.bat
@@ -1,3 +1,3 @@
 cd "G:\WORK\学习\法国\1. INSA\8. IF3\0. Divers\课程表\servif.insa-lyon.fr"
-node fetchEdT.js >> 更新课表.log && git add . >> 更新课表.log&& git commit -m "自动更新课表" >> 更新课表.log && git push >> 更新课表.log
+node fetchEdT.js >> 更新课表.log && git add "json/fetchtime.json" "EdT/3IF.html" >> 更新课表.log&& git commit -m "自动更新课表" >> 更新课表.log && git push >> 更新课表.log
 pause
